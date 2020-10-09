06025251e2aa9885a23f1d3161e3cab3e6f41a52
Shane Burgess <shane.burgess@gmail.com>
2020-10-09 15:10:11 -0400

    SB | Adds code to process the data

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,7 +1,16 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 
 export const getCommits = () => {
-  return axios.get(
-    'https://api.github.com/repos/theshane/github-viewer/commits',
-  );
+  return axios
+    .get('https://api.github.com/repos/theshane/github-viewer/commits')
+    .then((response: any) => {
+      const processed = response.map((resp: any) => {
+        return {
+          sha: resp.sha,
+          author: resp.commit.author,
+          message: resp.commit.message,
+        };
+      });
+      return processed;
+    });
 };
