0f06c8ff7255ca276eceeeaa75882c2672a7f5ce
Shane Burgess <shane.burgess@gmail.com>
2020-10-09 15:09:53 -0400

    SB | Adds moxios and uses that in the test and adds a test for processing the data

--- __tests__/service.spec.ts
+++ __tests__/service.spec.ts
@@ -1,6 +1,7 @@
 import {testCommits} from './fixtures/testData';
 import {getCommits} from '../src/services/httpService';
 import axios from 'axios';
+import moxios from 'moxios';
 
 const mockGet = jest
   .fn()
@@ -14,12 +15,50 @@ mockedAxios.get.mockResolvedValue(testCommits);
 
 describe('httpService', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    moxios.install();
   });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
   it('fetches the correct url', async () => {
-    await getCommits();
-    expect(mockedAxios.get).toHaveBeenCalledWith(
+    moxios.stubRequest(
       'https://api.github.com/repos/theshane/github-viewer/commits',
+      {
+        status: 200,
+        response: testCommits,
+      },
     );
+
+    const response = await getCommits();
+    expect(response).toBeDefined();
+  });
+
+  it('formats the data correctly', async () => {
+    const expectedData = [
+      {
+        sha: '0238d542faa7dcf91af9d31e8ce8c0a90b80a4c5',
+        author: {
+          name: 'Shane Burgess',
+          email: 'shane.burgess@gmail.com',
+          date: '2020-02-11T23:37:20Z',
+        },
+        message: 'Adds more to readme',
+      },
+    ];
+    moxios.stubRequest(
+      'https://api.github.com/repos/theshane/github-viewer/commits',
+      {
+        status: 200,
+        response: testCommits,
+      },
+    );
+    const response = await getCommits();
+    expect(response[0].sha).toBe(expectedData[0].sha);
+    expect(response[0].author.name).toBe(expectedData[0].author.name);
+    expect(response[0].author.email).toBe(expectedData[0].author.email);
+    expect(response[0].author.date).toBe(expectedData[0].author.date);
+    expect(response[0].message).toBe(expectedData[0].message);
   });
 });
