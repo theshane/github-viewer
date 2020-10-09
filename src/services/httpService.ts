ec2994dcf89e94b08cc9a64f0e1cffe131b5b693
Shane Burgess <shane.burgess@gmail.com>
2020-10-09 18:13:48 -0400

    SB | Fixes the processor

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,16 +1,23 @@
-import axios, {AxiosResponse} from 'axios';
+import axios from 'axios';
+
+export const processCommit = (commit: any) => {
+  return {
+    sha: commit.sha,
+    name: commit.commit.author.name,
+    message: commit.commit.message,
+  };
+};
 
 export const getCommits = () => {
   return axios
     .get('https://api.github.com/repos/theshane/github-viewer/commits')
     .then((response: any) => {
-      const processed = response.map((resp: any) => {
-        return {
-          sha: resp.sha,
-          author: resp.commit.author,
-          message: resp.commit.message,
-        };
+      const processed = response.data.map((commit: any) => {
+        return processCommit(commit);
       });
       return processed;
+    })
+    .catch((resp) => {
+      return resp;
     });
 };
