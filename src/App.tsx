14babdbdf4629f3b176d350226ee5d82039c0cf4
Shane Burgess <shane.burgess@gm.com>
2020-10-09 12:30:25 -0400

    SB | Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+export const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Hello</Text>
+      </SafeAreaView>
+    </>
+  );
+};
